1142a0f8282e805f644b6c6181ab55c3c52928bd
RGB\chris <christianevrhuetter@gmail.com>
2025-03-03 10:06:59 +0100

    black node at position 0,0,0 issue - wip

--- uploader.py
+++ uploader.py
@@ -237,7 +237,8 @@ def makeXYZTexture(project, pixeldata, name=None, latlon_flag=False):
 
                 texh[i] = pixelh
                 texl[i] = pixell
-                #print(pixelh)
+               
+                #print(pixelh) 
                 #print("C_DEBUG: normalized coordinates.")
 
         else:
@@ -263,7 +264,8 @@ def makeXYZTexture(project, pixeldata, name=None, latlon_flag=False):
 
                 texh[i] = pixelh
                 texl[i] = pixell
-                #print(pixelh)
+                
+                print(pixelh)
                 #print("C_DEBUG: DID NOT normalize coordinates.")
 
     new_imgh = Image.new('RGB', (128, hight))
